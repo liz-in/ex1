8a7c25a58c1d4fde04ffddadf1f00b254b515a0b
Liz Yan <lizyan@blueplanet.com.tw>
2020-02-10 17:44:50 +0800

    feat: count the words of search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,27 @@ export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
   imgurl = '/assets/images/logo.png';
+  count = 0;
   constructor() {
   }
   changeTitle(altKey: boolean) {
     if (altKey) {
-      this.title  = 'The Will Will Web';
+      this.title = 'The Will Will Web';
+    }
+  }
+  checkWord($event: KeyboardEvent) {
+    if ($event.key === 'Escape') {
+      this.countWord($event.target as HTMLInputElement, true);
+    } else {
+      this.countWord($event.target as HTMLInputElement, false);
+    }
+  }
+  countWord(element: HTMLInputElement, clear: boolean) {
+    if (clear) {
+      element.value = '';
+      this.count = 0;
+    } else {
+      this.count = element.value.length;
     }
   }
 }
